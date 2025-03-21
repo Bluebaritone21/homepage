1ce4550ea3de779514099b840500235cc53b3261
Bluebaritone21 <82056947+Bluebaritone21@users.noreply.github.com>
2025-03-21 22:57:36 +0000

    moved app definitions to seperate files

---
+++ webroot/~bb21/gnome/apps/texteditor.js
@@ -0,0 +1,14 @@
+function texteditor(){
+    var textbox=document.createElement(`textarea`);
+    textbox.style.width='100%';
+    textbox.style.height='calc(100% - 40px)';
+    textbox.style.resize='none';
+    var win = makeWindow(600,500,textbox);
+    win.style.backgroundColor='#eaeaea';
+    var title  =document.createElement('h3');
+    title.innerText='Untitled Document';
+    title.style.position='absolute';
+    title.style.top='-5px';
+    title.style.left='30%';
+    win.appendChild(title);
+}
